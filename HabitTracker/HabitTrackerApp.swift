bc2ff64b85ca23f698aa8e26f9699f7d23322500
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-05-07 19:54:34 +0200

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Gregoire Meyer on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
