f08e52b1df88a97eaa3b446d946941de2fc80bb4
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-05-08 12:45:49 +0200

    Add circular progress view

--- HabitTracker/ContentView.swift
+++ HabitTracker/ContentView.swift
@@ -14,9 +14,27 @@ struct ContentView: View {
     
     @State private var showingAddHabitSheet = false
     
+    @State private var dailyProgress = 0.0
+    
     var body: some View {
         NavigationStack {
             VStack {
+                ZStack {
+                    Circle()
+                        .stroke(.blue.opacity(0.5), lineWidth: 25)
+                        .frame(width: 200)
+                    
+                    Circle()
+                        .trim(from: 0, to: dailyProgress)
+                        .stroke(.blue, style: StrokeStyle(lineWidth: 25, lineCap: .round))
+                        .frame(width: 200)
+                        .rotationEffect(.degrees(-90))
+                        .animation(.easeOut(duration: 0.3), value: dailyProgress)
+                }
+                
+                Spacer()
+                    .frame(height: 50)
+                
                 List {
                     ForEach(habits) { habit in
                         HStack {
@@ -29,8 +47,10 @@ struct ContentView: View {
                             }
                         }
                     }
+                    .listRowBackground(Color.secondary.opacity(0.1))
                 }
                 .listRowSpacing(10)
+                .scrollContentBackground(.hidden)
             }
             .navigationTitle("Today")
             .toolbar {
